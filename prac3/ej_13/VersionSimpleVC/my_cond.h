997188bd26ab8b54029dead9e028ca682c5e3abd
Mirko Caballero <161973892+Mirko2612@users.noreply.github.com>
2026-05-29 23:19:12 -0300

    Inclusión del triple semáforo

--- prac3/ej_13/VersionSimpleVC/my_cond.h
+++ prac3/ej_13/VersionSimpleVC/my_cond.h
@@ -1,26 +1,29 @@
-#ifndef __MY_COND_H__
-#define __MY_COND_H__
+#ifndef __VCOND_H__
+#define __VCOND_H__
 
 #include <pthread.h>
 #include <semaphore.h>
-
-typedef struct _my_cond_t {
-    sem_t sem;
-} my_cond_t;
+// estructura de variable de condición hecha con semáforos.
+typedef struct _v_cond_t 
+{
+    sem_t wait, intr, sig;/*semáforos que representan: el esperado por el waiter, proteccion a la variable count_waiter 
+    * y el indicador si hubo un thread que haya recibido la signal*/
+    int cont_waiters; // contador de hilos esperando una señal.
+} v_cond_t;
 
 // Inicializa la variable de condición.
-void my_cond_init(my_cond_t *cond);
+void my_cond_init(v_cond_t *cond);
 
 // Espera a que signalice o broadcastee la variable cond.
-void my_cond_wait(my_cond_t *cond, pthread_mutex_t *mutex);
+void my_cond_wait(v_cond_t *cond, pthread_mutex_t *mutex);
 
 // Signaliza la varibale cond.
-void my_cond_signal(my_cond_t *cond);
+void my_cond_signal(v_cond_t *cond);
 
 // Broadcastea la variable cond.
-void my_cond_broadcast(my_cond_t *cond);
+void my_cond_broadcast(v_cond_t *cond);
 
 // Detruye la variable cond.
-void my_cond_destroy(my_cond_t *cond);
+void my_cond_destroy(v_cond_t *cond);
 
-#endif
+#endif // __VCOND_H__
