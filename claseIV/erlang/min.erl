8f9a0ba5a723fc8c991876a6ccf72415fc42c798
Mirko Caballero <161973892+Mirko2612@users.noreply.github.com>
2026-05-31 22:43:40 -0300

    Corrección min.erl

--- claseIV/erlang/min.erl
+++ claseIV/erlang/min.erl
@@ -1,2 +1,13 @@
--module(min)
--export([])
+-module(min).
+-export([min/1]).
+
+min([Hd]) -> Hd; %% caso base
+
+min([Hd|Tl]) ->
+    Rest = min(Tl), %% defino resto como el minimo de la lista sin el primer elemento, separo con "," ya que pertenece a la misma clausula
+    if
+        Hd < Rest -> %% si el header de la lista es menor al resto 
+            Hd; %% devuelvo el header y como termina una rama pongo ";"
+    true -> %% else
+        Rest %% devuelvo el resto si es menor y en el caso del else va sin nada
+    end. %% fin de la función va con .
